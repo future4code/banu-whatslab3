4f7b4647e5345eb999bd6213599aa466de70c371
Ketryn Silva <78522164+ketrynsdm@users.noreply.github.com>
2021-09-15 21:59:52 -0300

    whatsapp-incial
    # Whatsapp
    Box do usuário
    Box de mensagem
    Botão enviar
    Box central onde coloquei a imagem dentro

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import WhatUser from './WhatUser';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<WhatUser />, document.getElementById('root'));
